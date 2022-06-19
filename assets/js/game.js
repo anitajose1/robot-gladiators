0edbd746e2d23b25108bdf73795d181e631953b2
Anita Jose <anitajose119@gmail.com>
2022-06-19 15:45:35 -0400

    player name bug fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -172,8 +172,25 @@ var shop = function () {
     }
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+    
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
+/* END GAME FUNCTIONS */
+
+/* GAME INFORMATION / VARIABLES */
+
+// player information
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -204,7 +221,8 @@ var playerInfo = {
     }
 };
 
-// Create an array of enemy objects
+// enemy information
+// Create an array of enemy details
 var enemyInfo = [
     {
         name: "Roborto",
@@ -220,10 +238,10 @@ var enemyInfo = [
     }
 ];
 
-console.log(enemyNames);
-console.log(enemyNames.length);
-console.log(enemyNames[0]);
-console.log(enemyNames[3]);
+console.log(enemyInfo);
+console.log(enemyInfo[0]);
+console.log(enemyInfo[0].name);
+console.log(enemyInfo[0]['attack']);
 
 /* END GAME INFORMATION / VARIABLES */
 
